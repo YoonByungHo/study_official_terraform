51d86458685c73278255b8e9cfeba48ba75d3a65
윤병호(Byungho Yoon)/Cloud Eng.팀/SKP <byungho.y@sk.com>
2022-07-25 08:25:42 +0900

    add backend s3

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws"{
   region = "ap-northeast-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "do-not-delete-terraform-state-sot"
+    key = "terraform.tfstate"
+    region = "ap-northeast-2"
+    encrypt = true
+    dynamodb_table = "do-not-delete-terraform-state-lock"
+    acl = "bucket-owner-full-control"
+  }
+}
