b0a902242290ec60f341bf2c101a819c7323a0e4
윤병호(Byungho Yoon)/Cloud Eng.팀/SKP <byungho.y@sk.com>
2022-07-25 11:03:26 +0900

    Operation Timeouts

--- Resource_Blocks.tf
+++ Resource_Blocks.tf
@@ -29,12 +29,45 @@
 # }
 
 # - 생성 안되는 ami
+# data "aws_ami" "example" {
+#   most_recent      = true
+
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-arm64-*"]
+#   }
+# }
+
+# resource "aws_instance" "example" {
+#   instance_type = "t2.micro"
+#   ami           = data.aws_ami.example.id
+
+#   lifecycle {
+#     # The AMI ID must refer to an AMI that contains an operating system
+#     # for the `x86_64` architecture.
+#     precondition {
+#       condition     = data.aws_ami.example.architecture == "x86_64"
+#       error_message = "The selected AMI must be for the x86_64 architecture."
+#     }
+#   }
+# }
+
+# ------------------------------------------------------------------------------------------------------
+# Operation Timeouts
+# 일부 리소스 유형은 특정 작업이 실패한 것으로 간주되기 전에 허용되는 시간을 사용자 지정할 수 있는 특별한 시간 초과 중첩 블록 인수를 제공합니다. 
+# 예를 들어 aws_db_instance는 생성, 업데이트 및 삭제 작업에 대해 구성 가능한 시간 초과를 허용합니다.
+
+# 시간 초과는 공급자의 리소스 유형 구현에 의해 완전히 처리되지만 이러한 기능을 제공하는 리소스 유형은 구성 가능한 시간 초과 값이 있는 각 작업의 이름을 
+# 딴 중첩 인수가 있는 시간 초과라는 하위 블록을 정의하는 규칙을 따릅니다. 이러한 각 인수는 60분의 경우 "60m", 10초의 경우 "10s" 또는 2시간의 경우 "2h"와 같이 
+# 지속 시간의 문자열 표현을 사용합니다.
+
+# - 생성 시간이 1분으로 제한
 data "aws_ami" "example" {
   most_recent      = true
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-arm64-*"]
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-*"]
   }
 }
 
@@ -42,12 +75,8 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   ami           = data.aws_ami.example.id
 
-  lifecycle {
-    # The AMI ID must refer to an AMI that contains an operating system
-    # for the `x86_64` architecture.
-    precondition {
-      condition     = data.aws_ami.example.architecture == "x86_64"
-      error_message = "The selected AMI must be for the x86_64 architecture."
-    }
+  timeouts {
+    create = "1m"
+    # delete = "2h"
   }
 }
