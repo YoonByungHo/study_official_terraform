523a388bb094405756786e0821e8bc7ccb91ba49
윤병호(Byungho Yoon)/Cloud Eng.팀/SKP <byungho.y@sk.com>
2022-07-30 17:20:42 +0900

    Declaring Provisioners

--- Declaring_Provisioners.tf
+++ Declaring_Provisioners.tf
@@ -1,30 +1,31 @@
 # How to use Provisioners
 
 
-data "aws_ami" "example" {
-  most_recent      = true
+# data "aws_ami" "example" {
+#   most_recent      = true
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-*"]
-  }
-}
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-*"]
+#   }
+# }
 
-resource "aws_instance" "server" {
+# # 생성된 EC2에서 local-exec가 실행됨
+# resource "aws_instance" "server" {
 
-  ami           = data.aws_ami.example.id
-  instance_type = "t2.micro"
+#   ami           = data.aws_ami.example.id
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "Server"
-  }
-  lifecycle {
-    precondition {
-        condition     = data.aws_ami.example.architecture == "x86_64"
-        error_message = "The selected AMI must be for the x86_64 architecture."
-    }
-  }
-  provisioner "local-exec" {
-    command = "echo The servers IP address is ${self.private_ip} >> ~/test.txt"
-  }
-}
+#   tags = {
+#     Name = "Server"
+#   }
+#   lifecycle {
+#     precondition {
+#         condition     = data.aws_ami.example.architecture == "x86_64"
+#         error_message = "The selected AMI must be for the x86_64 architecture."
+#     }
+#   }
+#   provisioner "local-exec" {
+#     command = "echo The servers IP address is ${self.private_ip} >> ~/test.txt"
+#   }
+# }
