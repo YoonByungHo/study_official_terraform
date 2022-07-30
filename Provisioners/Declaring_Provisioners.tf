c076e28ee591985b9325e44b5562f9732a1c92a1
윤병호(Byungho Yoon)/Cloud Eng.팀/SKP <byungho.y@sk.com>
2022-07-30 16:48:34 +0900

    Declaring Provisioners

---
+++ Provisioners/Declaring_Provisioners.tf
@@ -0,0 +1,30 @@
+# How to use Provisioners
+
+
+data "aws_ami" "example" {
+  most_recent      = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-*"]
+  }
+}
+
+resource "aws_instance" "server" {
+
+  ami           = data.aws_ami.example.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Server"
+  }
+  lifecycle {
+    precondition {
+        condition     = data.aws_ami.example.architecture == "x86_64"
+        error_message = "The selected AMI must be for the x86_64 architecture."
+    }
+  }
+  provisioner "local-exec" {
+    command = "echo The server's IP address is ${self.private_ip}"
+  }
+}
