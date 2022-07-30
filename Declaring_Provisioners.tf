196a608d14f146045bc243b204f8632674558491
윤병호(Byungho Yoon)/Cloud Eng.팀/SKP <byungho.y@sk.com>
2022-07-31 08:19:48 +0900

    Provisioner Connection Settings

--- Declaring_Provisioners.tf
+++ Declaring_Provisioners.tf
@@ -29,3 +29,57 @@
 #     command = "echo The servers IP address is ${self.private_ip} >> ~/test.txt"
 #   }
 # }
+
+# # Destroy-Time Provisioners
+# data "aws_ami" "example" {
+#   most_recent      = true
+
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220606.1-x86_64-*"]
+#   }
+# }
+
+# # 삭제될 때 EC2에서 local-exec가 실행됨
+# resource "aws_instance" "server" {
+
+#   ami           = data.aws_ami.example.id
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "Server"
+#   }
+#   lifecycle {
+#     precondition {
+#         condition     = data.aws_ami.example.architecture == "x86_64"
+#         error_message = "The selected AMI must be for the x86_64 architecture."
+#     }
+#   }
+#   provisioner "local-exec" {
+#     when = destroy
+#     command = "echo The servers IP address is ${self.private_ip}"
+#   }
+# }
+
+# Failure Behavior
+
+# 
+# resource "aws_instance" "server" {
+
+#   ami           = data.aws_ami.example.id
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "Server"
+#   }
+#   lifecycle {
+#     precondition {
+#         condition     = data.aws_ami.example.architecture == "x86_64"
+#         error_message = "The selected AMI must be for the x86_64 architecture."
+#     }
+#   }
+#   provisioner "local-exec" {
+#     on_failure = fail
+#     command = "echo The servers IP address is ${self.private_ip}"
+#   }
+# }
---
+++ Provisioner_Connection_Settings.tf
@@ -0,0 +1,115 @@
+# Connection Block
+
+# 원격 리소스에 액세스하는 방법을 설명하는 하나 이상의 연결 블록을 만들 수 있습니다. 
+# 다중 연결을 제공하는 한 가지 사용 사례는 초기 제공자가 루트 사용자로 연결하여 사용자 계정을 설정하도록 한 다음 후속 제공자가 더 제한된 권한을 가진 사용자로 연결하도록 하는 것입니다.
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name      = "name"
+    values    = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+  owners      = ["amazon"]
+}
+
+resource "aws_security_group" "example" {
+  name        = "${var.name}-${var.env}-example-sg"
+  description = "example"
+  vpc_id      = "vpc-07fcd32c280fc8256"
+
+  tags                      = var.tf_tag
+}
+
+resource "aws_security_group_rule" "inbound_example_22" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+
+  security_group_id = aws_security_group.example.id
+  source_security_group_id = aws_security_group.example.id
+  depends_on = [
+    aws_security_group.example,
+  ]
+}
+
+resource "aws_security_group_rule" "outbound_any" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+
+  depends_on = [
+    aws_security_group.example,
+  ]
+}
+
+resource "aws_iam_role" "example" {
+  name               = "${var.name}-${var.env}-example-role"
+  path               = "/"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+  tags  = var.tf_tag
+}
+
+resource "aws_iam_instance_profile" "ssm_profile" {
+  name = "${var.name}-${var.env}-ssm-profile"
+  role = "${aws_iam_role.ssm_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_core_attach_ssm_role" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = data.aws_iam_policy.ssm_core.arn
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.micro"
+  iam_instance_profile  = aws_iam_instance_profile.example.id
+  security_groups = [ aws_security_group.example.id ]
+  subnet_id = "subnet-0150c5d47b59b23a5"
+  vpc_security_group_ids = [ aws_security_group.example.id ]
+
+  depends_on = [
+    aws_security_group.example,
+    aws_iam_role.example
+  ]
+
+  tags                      = {
+      "Name" = "${var.name}-${var.env}-example1"
+  }
+}
+
+resource "aws_instance" "example2" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.micro"
+  iam_instance_profile  = aws_iam_instance_profile.example.id
+  security_groups = [ aws_security_group.example.id ]
+  subnet_id = "subnet-0150c5d47b59b23a5"
+  vpc_security_group_ids = [ aws_security_group.example.id ]
+
+  depends_on = [
+    aws_security_group.example,
+    aws_iam_role.example
+  ]
+
+  tags                      = {
+      "Name" = "${var.name}-${var.env}-example2"
+  }
+}
