72014aee3431fae2bbbe39593ce0782bf15106b9
윤병호(Byungho Yoon)/Cloud Eng.팀/SKP <byungho.y@sk.com>
2022-08-02 20:46:21 +0900

    Provisioner Connection Settings

--- Provisioner_Connection_Settings.tf
+++ Provisioner_Connection_Settings.tf
@@ -108,20 +108,20 @@ EOF
   }
 }
 
-resource "aws_instance" "example2" {
-  ami           = data.aws_ami.amazon_linux_2.id
-  instance_type = "t3.micro"
-  iam_instance_profile  = aws_iam_instance_profile.example.id
-  security_groups = [ aws_security_group.example.id ]
-  subnet_id = "subnet-0150c5d47b59b23a5"
-  vpc_security_group_ids = [ aws_security_group.example.id ]
+# resource "aws_instance" "example2" {
+#   ami           = data.aws_ami.amazon_linux_2.id
+#   instance_type = "t3.micro"
+#   iam_instance_profile  = aws_iam_instance_profile.example.id
+#   security_groups = [ aws_security_group.example.id ]
+#   subnet_id = "subnet-0150c5d47b59b23a5"
+#   vpc_security_group_ids = [ aws_security_group.example.id ]
 
-  depends_on = [
-    aws_security_group.example,
-    aws_iam_role.example
-  ]
+#   depends_on = [
+#     aws_security_group.example,
+#     aws_iam_role.example
+#   ]
 
-  tags                      = {
-      "Name" = "example2"
-  }
-}
+#   tags                      = {
+#       "Name" = "example2"
+#   }
+# }
