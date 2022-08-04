a503518477bafdccb7ca420c8d3254369c6e3f74
윤병호(Byungho Yoon)/Cloud Eng.팀/SKP <byungho.y@sk.com>
2022-08-04 09:08:08 +0900

    Provisioners Without a Resource

--- Provisioners_Without_Resource.tf
+++ Provisioners_Without_Resource.tf
@@ -116,8 +116,13 @@ resource "null_resource" "example" {
     cluster_instance_ids = "${join(",", aws_instance.example.*.id)}"
   }
   connection {
-    host = "${element(aws_instance.cluster.*.public_ip, 0)}"
+    host = "${element(aws_instance.example.*.public_ip, 0)}"
   }
-
+  provisioner "remote-exec" {
+    # Bootstrap script called with private_ip of each node in the cluster
+    inline = [
+      "bootstrap-cluster.sh ${join(" ", aws_instance.example.*.private_ip)}",
+    ]
+  }
 }
 
